252f73f3e458e4cea6f81a6db5068791839ea344
yukidaruman883-coder <yukidaruman883@gmail.com>
2026-07-13 19:57:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+/* 逆時計 Service Worker */
+var CACHE_NAME = "reverse-clock-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-512-maskable.png",
+  "./icons/icon-180.png"
+];
+
+// インストール時に全アセットをキャッシュ
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+// 旧バージョンのキャッシュを削除
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== CACHE_NAME; })
+            .map(function (key) { return caches.delete(key); })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+// キャッシュ優先・なければネットワーク(オフラインでも起動可能)
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      });
+    })
+  );
+});
